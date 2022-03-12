7dfc9367903e5d699132a42ae5ab248f2d31c023
EnokiUN <minekillergaming1@gmail.com>
2022-03-12 15:54:42 +0300

    brb eat

---
+++ pyvolt/client.py
@@ -0,0 +1,13 @@
+import aiohttp
+
+class Client:
+    """
+    desription here
+    """
+    def __init__(self):
+        """
+        desription here
+        """
+        self.ws = None
+        self.http = None
+        self.listeners = {}
