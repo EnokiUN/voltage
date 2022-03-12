9f3aa6a51f6a7dbe1e0369e42202c50eee047190
EnokiUN <minekillergaming1@gmail.com>
2022-03-13 00:42:53 +0300

    Updated res.py to reflect new syntax

--- res.py
+++ res.py
@@ -1,13 +1,12 @@
-import imp
-
-
 import pyvolt
 
 client = pyvolt.Client()
 
-@client.listen("message")
-def on_message(payload):
-    if payload["content"] == "ping":
-        client.send_message(payload["channel"], "Hello World!", [{"id": payload["_id"], "mention": True}], [{"type": "text", "title": "Hello World!", "description": "This is an embed!", "color": 0xFF0000}])
+@client.listen("message", raw=True)
+async def on_message(payload):
+    if payload["content"] == "-ping":
+        await client.http.send_message(payload["channel"], "pong")
+    if payload["content"] == "-embed":
+        await client.http.send_message(payload["channel"], "h", embeds=[{"title": "h"}])
 
-client.run("")
+client.run("TOKEN")
