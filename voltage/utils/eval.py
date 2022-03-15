0b9b40a80bc5487508b8b424b06bcfe936fa0c72
EnokiUN <minekillergaming1@gmail.com>
2022-03-15 23:31:11 +0300

    Merge branch 'main' of https://github.com/EnokiUN/voltage

--- voltage/utils/eval.py
+++ voltage/utils/eval.py
@@ -1,17 +1,21 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING, Optional
+
 from re import sub
+from typing import TYPE_CHECKING, Optional
 
 if TYPE_CHECKING:
-    from ..internals import HTTPHandler
     from .. import Client
+    from ..internals import HTTPHandler
     from ..types import MessagePayload
 
-async def eval(client: Client, message: MessagePayload, superuser_ids: list[str], message_start: Optional[str] = "!eval"):
+
+async def eval(
+    client: Client, message: MessagePayload, superuser_ids: list[str], message_start: Optional[str] = "!eval"
+):
     """
     A function that provides a very basic asynchronous eval function.
     DANGER: THIS FUNCTION CAN PUT YOUR HOST MACHINE IN DANGER SO BE CAREFUL OF WHO USES IT AND HOW IT IS USED.
-    
+
     Parameters
     ----------
     client: pyvolt.Client
@@ -28,7 +32,10 @@ async def eval(client: Client, message: MessagePayload, superuser_ids: list[str]
         if message["author"] not in superuser_ids:
             return await client.http.send_message(message["channel"], "You don't have access to this command.")
         if not (parts := message["content"].split(" ")) or not (len(parts) > 1):
-            return await client.http.send_message(message["channel"], f"Usage: {message_start} <code>\nYou also have to be a superuser to use this command.")
+            return await client.http.send_message(
+                message["channel"],
+                f"Usage: {message_start} <code>\nYou also have to be a superuser to use this command.",
+            )
         content = parts[1]
         content = sub("```python|```py|```", "", content)
         lines = content.splitlines()
@@ -39,6 +46,8 @@ async def eval(client: Client, message: MessagePayload, superuser_ids: list[str]
         try:
             res = await locals()["__eval__func"](client, message, client.http)
             if res:
-                await client.http.send_message(message["channel"], str(res).replace(client.http.token, "[TOKEN REDACTED]"))
+                await client.http.send_message(
+                    message["channel"], str(res).replace(client.http.token, "[TOKEN REDACTED]")
+                )
         except Exception as e:
             await client.http.send_message(message["channel"], str(e))
