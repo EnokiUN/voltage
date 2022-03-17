30f19ec8d5fe61241775439e4cf05d69797b055e
EnokiUN <minekillergaming1@gmail.com>
2022-03-17 15:50:58 +0300

    added invites

---
+++ voltage/invites.py
@@ -0,0 +1,91 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from .asset import Asset
+
+if TYPE_CHECKING:
+    from .types import InvitePayload, PartialInvitePayload
+    from .internals import CacheHandler
+
+
+class Invite:
+    """
+    A class which represents a Voltage invite.
+
+    Attributes
+    ----------
+    code: :class:`str`
+        The invite code.
+    type: :class:`str`
+        The invite type.
+    payload: :class:`InvitePayload`
+        The invite payload.
+    server_id: :class:`str`
+        The server ID.
+    server: :class:`Server`
+        The server the invite is for.
+    channel_id: :class:`str`
+        The channel ID.
+    channel: :class:`Channel`
+        The channel the invite is for.
+    member_count: :class:`int`
+        The member count.
+    author_name: :class:`str`
+        The author name.
+    user: :class:`User`
+        The user who created the invite.
+    avatar: :class:`Asset`
+        The avatar of the user who created the invite.
+    """
+    __slots__ = ('code', 'type', 'payload', 'server_id', 'server', 'channel_id', 'channel', 'member_count', 'user', 'cache')
+    
+    def __init__(self, data: InvitePayload, code: str, cache: CacheHandler):
+        self.code = code
+        self.type = data['type']
+        self.payload = data
+
+        self.server_id = data['serer_id']
+        self.server = cache.get_server(self.server_id)
+
+        self.channel_id = data['channel_id']
+        self.channel = cache.get_channel(self.channel_id)
+        self.member_count = data['member_count']
+
+        self.user = cache.get_user(data['user_name'], "name")
+
+        self.cache = cache
+
+    @staticmethod
+    def from_partial(code: str, data: PartialInvitePayload, cache: CacheHandler) -> Invite:
+        """
+        A utility function that creates an Invite object from a partial payload.
+
+        Parameters
+        ----------
+        code: :class:`str`
+            The invite code.
+        data: :class:`PartialInvitePayload`
+            The partial payload.
+        cache: :class:`CacheHandler`
+            The cache handler.
+        """
+        self = Invite.__new__(Invite)
+
+        self.code = code
+        self.paylaod = data
+        self.cache = cache
+        self.type = "Server"
+
+        self.server_id = data['server']
+        self.server = cache.get_server(self.server_id)
+        self.member_count = len(self.server.members)
+
+        self.channel_id = data['channel']
+        self.channel = cache.get_channel(self.channel_id)
+
+        self.user = cache.get_user(data['creator'])
+
+        return self
+
+    async def delete(self):
+        return await self.cache.http.delete_invite(self.code)
