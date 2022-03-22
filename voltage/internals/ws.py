39f59be5e8bbb4dd3b692b4ab91cd3fb9a6cbf67
EnokiUN <minekillergaming1@gmail.com>
2022-03-22 21:09:57 +0300

    added websocket events

--- voltage/internals/ws.py
+++ voltage/internals/ws.py
@@ -3,11 +3,15 @@ from __future__ import annotations
 from asyncio import get_event_loop, sleep
 from json import loads
 from typing import TYPE_CHECKING, Any, Callable, Dict
+from copy import copy
+
+from ..enums import RelationshipType
 
 if TYPE_CHECKING:
     from aiohttp import ClientSession, ClientWebSocketResponse
+    from ..channels import GroupDMChannel
 
-    from ..types import OnMessagePayload, OnReadyPayload
+    from ..types.ws import *
     from .cache import CacheHandler
     from .http import HTTPHandler
 
@@ -92,6 +96,12 @@ class WebSocketHandler:
         if func := getattr(self, f"handle_{event}", None):
             await func(payload)
 
+    async def handle_authenticated(self, _):
+        """
+        Handles the authenticated event.
+        """
+        pass
+
     async def handle_ready(self, payload: OnReadyPayload):
         """
         Handles the ready event.
@@ -104,3 +114,165 @@ class WebSocketHandler:
         Handles the message event.
         """
         await self.dispatch("message", self.cache.add_message(payload))
+
+    async def handle_messageupdate(self, payload: OnMessageUpdatePayload):
+        """
+        Handles the message update event.
+        """
+        self.dispatch("raw_message_update", payload)
+
+        try:
+            message = self.cache.get_message(payload["id"])
+            old = copy(message)
+            message._update(payload)
+            await self.dispatch("message_update", old, message)
+        except KeyError:
+            return
+
+    async def handle_messagedelete(self, payload: OnMessageDeletePayload):
+        """
+        Handles the message delete event.
+        """
+        self.dispatch("raw_message_delete", payload)
+
+        try:
+            message = self.cache.get_message(payload["id"])
+            self.cache.messages.pop(message.id)
+            await self.dispatch("message_delete", message)
+        except KeyError:
+            return
+        
+    async def handle_channelcreate(self, payload: OnChannelCreatePayload):
+        """
+        Handles the channel create event.
+        """
+        await self.dispatch("channel_create", self.cache.add_channel(payload))
+
+    async def handle_channelupdate(self, payload: OnChannelUpdatePayload):
+        """
+        Handles the channel update event.
+        """
+        channel = self.cache.get_channel(payload["id"])
+        old = copy(channel)
+        channel._update(payload)
+        await self.dispatch("channel_update", old, channel)
+
+    async def handle_channeldelete(self, payload: OnChannelDeletePayload):
+        """
+        Handles the channel delete event.
+        """
+        channel = self.cache.get_channel(payload["id"])
+        self.cache.channels.pop(channel.id)
+        await self.dispatch("channel_delete", channel)
+
+    async def handle_groupchanneljoin(self, payload):
+        """
+        Handles the group channel join event.
+        """
+        channel = self.cache.get_channel(payload['id'])
+        user = self.cache.get_user(payload['user'])
+        if isinstance(channel, GroupDMChannel):
+            channel.add_recepient(user)
+            await self.dispatch('group_channel_join', channel, user)
+
+    async def handle_groupchannelleave(self, payload):
+        """
+        Handles the group channel leave event.
+        """
+        channel = self.cache.get_channel(payload['id'])
+        user = self.cache.get_user(payload['user'])
+        if isinstance(channel, GroupDMChannel):
+            channel.remove_recepient(user)
+            await self.dispatch('group_channel_leave', channel, user)
+
+    async def handle_channelstarttyping(self, payload: OnChannelStartTypingPayload):
+        """
+        Handles the channel start typing event.
+        """
+        channel = self.cache.get_channel(payload['id'])
+        user = self.cache.get_user(payload['user'])
+        await self.dispatch('channel_start_typing', channel, user)
+
+    async def handle_channelstoptyping(self, payload: OnChannelDeleteTypingPayload):
+        """
+        Handles the channel stop typing event.
+        """
+        channel = self.cache.get_channel(payload['id'])
+        user = self.cache.get_user(payload['user'])
+        await self.dispatch('channel_stop_typing', channel, user)
+
+    async def handle_serverupdate(self, payload: OnServerUpdatePayload):
+        """
+        Handles the server update event.
+        """
+        server = self.cache.get_server(payload["id"])
+        old = copy(server)
+        server._update(payload)
+        await self.dispatch("server_update", old, server)
+
+    async def handle_serverdelete(self, payload: OnServerDeletePayload):
+        """
+        Handles the server delete event.
+        """
+        server = self.cache.get_server(payload["id"])
+        self.cache.servers.pop(server.id)
+        await self.dispatch("server_delete", server)
+
+    async def handle_servermemberupdate(self, payload: OnServerMemberUpdatePayload):
+        """
+        Handles the server member update event.
+        """
+        server = self.cache.get_server(payload["id"]["server"])
+        member = server.get_member(server.id)
+        if member:
+            old = copy(member)
+            member._update(payload)
+            await self.dispatch("server_member_update", old, member)
+
+    async def handle_servermemberjoin(self, payload: OnServerMemberJoinPayload):
+        """
+        Handles the server member join event.
+        """
+        member = self.cache.add_member(payload["id"], {"_id": {"server": payload["id"], "user": payload["user"]}})
+        await self.dispatch("server_member_join", member)
+
+    async def handle_memberleave(self, payload: OnServerMemberLeavePayload):
+        """
+        Handles the member leave event.
+        """
+        server = self.cache.get_server(payload["id"])
+        member = server.get_member(payload["user"])
+        if member:
+            server.members.remove(member)
+            await self.dispatch("member_leave", member)
+
+    async def handle_serverroleupdate(self, payload: OnServerRoleUpdatePayload):
+        """
+        Handles the server role update event.
+        """
+        server = self.cache.get_server(payload["id"])
+        role = server.get_role(payload["role_id"])
+        if role:
+            old = copy(role)
+            role._update(payload)
+            await self.dispatch("server_role_update", old, role)
+            
+    async def handle_serverroledelete(self, payload: OnServerRoleDeletePayload):
+        """
+        Handles the server role delete event.
+        """
+        server = self.cache.get_server(payload["id"])
+        role = server.get_role(payload["role_id"])
+        if role:
+            server.roles.remove(role)
+            await self.dispatch("server_role_delete", role)
+
+    async def handle_userupdate(self, payload: OnUserUpdatePayload):
+        """
+        Handles the user update event.
+        """
+        user = self.cache.get_user(payload["id"])
+        old = copy(user)
+        user._update(payload)
+        await self.dispatch("user_update", old, user)
+
