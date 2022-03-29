3f2f6a5d64c45155cfda3ffe4f9d423e88764a0c
b1ue64 <77976308+b1ue64@users.noreply.github.com>
2022-03-29 17:47:19 +0000

    Use more arguments in the embed example

--- examples/embed.py
+++ examples/embed.py
@@ -3,13 +3,21 @@ import voltage  # Import voltage.
 client = voltage.Client()  # Initialize the client.
 
 
-@client.listen("message")
-async def on_message(message):  # Doesn't matter what you call the function.
-    if message.content.startswith("-ping"):
-        embed = voltage.SendableEmbed(title="Cat", description="Cat", url="https://http.cat/200")
+@client.listen("message") # Listen for pings
+async def on_ping(message):  # Doesn't matter what you call the function.
+    if message.content.startswith("embed"):
+        # Create the embed. Most of these attributes are self-explanitory
+        embed = voltage.SendableEmbed(
+            title="Cat", # The title of the embed
+            description="Cat", # The description of the embed
+            colour="#DEADBF", # The colour of the "strip" at the side of the embed
+            icon_url=message.author.display_avatar.url, # The icon beside the title of the embed. "message.author.display_avatar.url" gets the user's avatar.
+            media="https://http.cat/200" # The media for the embed. Here, we have an image.
+        )
         # Reply to a message.
         await message.reply(content="Cat", embed=embed)  # You can use "#" if you don't want any content.
 
 
-# Run the client which is an abstraction of calling the start coroutine.
-client.run("TOKEN")  # Replace with your token.
+# Run the client
+client.run("TOKEN") # Replace with your own token
+
