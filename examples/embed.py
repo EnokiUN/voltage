998857f5eaa4861a01ee42c37c240ba4ce0d5a01
RGBCube <78925721+RGBCube@users.noreply.github.com>
2022-03-29 20:07:17 +0300

    There is no event called ping

--- examples/embed.py
+++ examples/embed.py
@@ -3,12 +3,13 @@ import voltage  # Import voltage.
 client = voltage.Client()  # Initialize the client.
 
 
-@client.listen("ping") # Listen for pings
-async def on_ping(message):  # Doesn't matter what you call the function.
-    embed = voltage.SendableEmbed(title="Cat", description="Cat", url="https://http.cat/200")
-    # Reply to a message.
-    await message.reply(content="Cat", embed=embed)  # You can use "#" if you don't want any content.
+@client.listen("message")
+async def on_message(message):  # Doesn't matter what you call the function.
+    if message.content.startswith("-ping"):
+        embed = voltage.SendableEmbed(title="Cat", description="Cat", url="https://http.cat/200")
+        # Reply to a message.
+        await message.reply(content="Cat", embed=embed)  # You can use "#" if you don't want any content.
 
 
-# Run the client
+# Run the client which is an abstraction of calling the start coroutine.
 client.run("TOKEN")  # Replace with your token.
