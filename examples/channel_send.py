1cc31d4c62c5adf81d1a6b07f7fcddd06b8e5927
YoniScripts <itz.kqlvin@gmail.com>
2022-04-02 10:22:34 -0400

    New example

--- examples/channel_send.py
+++ examples/channel_send.py
@@ -4,7 +4,7 @@ client = voltage.Client()  # Initialize the client.
 
 
 @client.listen("message")  # Listen for message.
-async def suggestion(message):  # Doesn't matter what you call the function.
+async def wakeup(message):  # Doesn't matter what you call the function.
     if message.content == "Wake up Enoki!":
         channel = client.get_channel("YOUR CHANNEL UUID") # You get the channel.
         await channel.send("Enoki wake up!") # Sends "Enoki wake up!" to the channel.
