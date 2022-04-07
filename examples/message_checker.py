c3939d35126d9eea114b4caf8f2ca20a3915a636
ks <92867218+ksIsCute@users.noreply.github.com>
2022-04-07 08:48:13 -0400

    Update message_checker.py

--- examples/message_checker.py
+++ examples/message_checker.py
@@ -5,7 +5,7 @@ client = voltage.Client() # initialize our client
 @client.listen('message') # specify what we're listening to
 async def on_message(message): # the name for this can be anything you want it to be
   ungodly_words = ["sus", "baka", "suppose", "real", "amogus"] # create our list to iterate through later
-  if message.content.lower() in ungodly_words: # run the if statement to trigger if a message is in the array
+  if ungodly_words in message.content.lower(): # run the if statement to trigger if a message is in the array
     await message.reply("*GASP!* You can't say that!") # reply to the message sent
     await message.delete() # delete the message afterwards
 
