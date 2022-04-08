d4b30d7152068d1c69d4c25329b8afb296d7d913
Yoni <75043612+yonipy@users.noreply.github.com>
2022-04-08 16:45:37 -0400

    yes

--- examples/cogs.py
+++ examples/cogs.py
@@ -1,12 +1,13 @@
-import voltage # Import voltage.
-from voltage.ext import commands # Import the framework.
+import voltage  # Import voltage.
+from voltage.ext import commands  # Import the framework.
 
-def setup(client) -> commands.Cog: # Setting up the cog.
-    test = commands.Cog("Test", "Some commands for testing.") # You may edit the description however you want.
 
-    @test.command() # Setting up a cog comamnd.
+def setup(client) -> commands.Cog:  # Setting up the cog.
+    test = commands.Cog("Test", "Some commands for testing.")  # You may edit the description however you want.
+
+    @test.command()  # Setting up a cog comamnd.
     async def ping(ctx):
         """Sends Pong!"""
-        await ctx.reply("Pong") # Sends Pong.
+        await ctx.reply("Pong")  # Sends Pong.
 
-    return test # Returning the cogs.
+    return test  # Returning the cogs.
