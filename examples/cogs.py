b029a55dc6ebb221ee7c49a5d9a80852250aa6d8
YoniScripts <itz.kqlvin@gmail.com>
2022-04-08 16:10:52 -0400

    cog

--- examples/cogs.py
+++ examples/cogs.py
@@ -1,12 +1,13 @@
-import voltage 
+import voltage
 from voltage.ext import commands
 
+
 def setup(client) -> commands.Cog:
-    test = commands.Cog("Test", "Some commands for testing.") # You may edit the description however you want.
+    test = commands.Cog("Test", "Some commands for testing.")  # You may edit the description however you want.
 
     @test.command()
     async def ping(ctx):
         """Sends Pong!"""
-        await ctx.reply("Pong") 
+        await ctx.reply("Pong")
 
-    return test # You returning the cogs.
+    return test  # You returning the cogs.
