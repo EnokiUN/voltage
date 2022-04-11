18e3f1863483bc5cea83961b6f47824c556b9bbe
Enoki <enokiun@gmail.com>
2022-04-11 17:57:16 +0300

    improved

--- examples/cogs.py
+++ examples/cogs.py
@@ -1,5 +1,7 @@
 import voltage  # Import voltage.
-from voltage.ext import commands  # Importing the commands framework so we that we're able to create a Cog object.
+from voltage.ext import (
+    commands,  # Importing the commands framework so we that we're able to create a Cog object.
+)
 
 
 # Next up is the setup function, aka where all the magic happens.
@@ -9,18 +11,17 @@ from voltage.ext import commands  # Importing the commands framework so we that 
 # The client is passed by default however.
 def setup(client) -> commands.Cog:
 
-    test = commands.Cog( # Create a new Cog object.
-        "Test", # Give it a name.
-        "Some commands for testing." # And an optional description.
+    test = commands.Cog(  # Create a new Cog object.
+        "Test", "Some commands for testing."  # Give it a name.  # And an optional description.
     )  # The name and description will be used in the help command.
 
     # Register a command as normal, difference is you use the Cog object instead of the client in the decorator.
     @test.command()
-    async def ping(ctx): # No self parameter.
+    async def ping(ctx):  # No self parameter.
         """Sends Pong!"""
         await ctx.reply("Pong from inside a Cog!")
 
-    return test # Finally, return the cog object.
+    return test  # Finally, return the cog object.
+
 
 # To load a Cog to the client you run `client.add_extension(path, *args, **kwargs)` with the path being the Python dotpath to your file, args and kwargs are optional.
-
