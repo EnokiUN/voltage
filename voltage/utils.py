867c90b888b8b7085ae1941a0feca3a34ddbb0df
EnokiUN <enokiun@gmail.com>
2022-04-11 23:16:08 +0300

    improvements

--- voltage/utils.py
+++ voltage/utils.py
@@ -1,12 +1,12 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING, Any, Callable, TypeVar, Optional
+from typing import TYPE_CHECKING, Any, Callable, TypeVar, Optional, Iterable
 
 if TYPE_CHECKING:
     pass
 
 T = TypeVar("T")
 
-def get(base: list[T], predicate: Callable[[T], bool]) -> Optional[T]:
+def get(base: Iterable[T], predicate: Callable[[T], bool]) -> Optional[T]:
     for item in base:
         if predicate(item):
             return item
