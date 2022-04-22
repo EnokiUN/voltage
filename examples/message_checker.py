a8d7094f4581121599be741084c6b6a6fa79d871
ks <92867218+ksIsCute@users.noreply.github.com>
2022-04-22 13:27:10 -0400

    Update message_checker.py

--- examples/message_checker.py
+++ examples/message_checker.py
@@ -8,17 +8,15 @@ ungodly_words = ["sus", "baka", "suppose", "real", "amogus"] # create our list t
 @client.listen('message') # specify what we're going to listen to
 async def on_message(message): # the name for this can be anything you want it to be
   if any([word in message.content.lower() for word in ungodly words]): # run the if statement to trigger if the message has words in the array
-    await message.reply("*GASP!* You can't say that word!") # reply to the message sent
-    await message.delete() # delete the message afterwards
-  elif message.content == "<@Your Bots ID>":
-    message.reply("Pong!")
+    await message.reply("*GASP!* You can't say that word!", delete_after=5) # reply to the message sent and delete OUR message after 5 seconds
+    await message.delete() # delete the USERS message
   await bot.handle_commands(message) 
   # Handle afterwards so other commands will work after the on_message, 
 
 @commands.command(name="add_word") # Create our command
 async def add_word(ctx, word): # Define our command
   ungodly_words.append(word.lower()) # Append to our list
-  await ctx.send(f"Added `{word.lower()}` to the list of words!") # Tell user that the word was added to the list.
+  await ctx.send(f"Added `{word.lower()}` to the list of `{len(ungodly_words)}` words!") # Tell user that the word was added to the list.
   
   
-client.run("TOKEN") # run your bot
+client.run("TOKEN") # Run the bot
