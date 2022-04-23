7f02d5ebed233aa7e6c0a813801da6f19b0affb0
EnokiUN <enokiun@gmail.com>
2022-04-23 17:56:40 +0300

    bumped version for pypi release

--- voltage/__init__.py
+++ voltage/__init__.py
@@ -53,7 +53,7 @@ __title__ = "Voltage"
 __author__ = "EnokiUN"
 __license__ = "MIT"
 __copyright__ = "Copyright (c) 2021-present EnokiUN"
-__version__ = "0.1.5a3"
+__version__ = "0.1.5a4"
 
 from .asset import Asset, PartialAsset
 from .categories import Category
