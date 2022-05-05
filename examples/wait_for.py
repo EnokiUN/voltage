e78bb6819fbd57d5ecba16ab5867371db6b83da5
ks <92867218+ksIsCute@users.noreply.github.com>
2022-05-05 14:39:32 -0400

    Update wait_for.py
    change message variable

--- examples/wait_for.py
+++ examples/wait_for.py
@@ -8,7 +8,7 @@ client = voltage.Client() # Initializing the client
 async def on_message(message): # The name for this function doesnt matter, but we'll be naming it on_message for this example.
   if message.content.lower() == "-send":
     await message.reply("Send me something nice!")
-    messagegiven = await bot.wait_for("message", check=lambda message: message.author.id != bot.user.id, timeout=30) # Assign this to a variable as it returns a Messageable object for later.
-    await message.reply(f"I appreciate the message, {message.author.name}, ill remember your words.. `{messagegiven.content}`") # Replying with the message content sent by the user.
+    msg = await bot.wait_for("message", check=lambda message: message.author.id != bot.user.id, timeout=30) # Assign this to a variable as it returns a Messageable object for later.
+    await message.reply(f"I appreciate the message, {message.author.name}, ill remember your words.. `{msg.content}`") # Replying with the message content sent by the user.
 
 client.run("token") # Replace with your token string and run!
