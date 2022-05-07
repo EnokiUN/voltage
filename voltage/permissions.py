f7f9f147666d2767c1b669b795e8d33c9c5117dc
Enoki <enokiun@gmail.com>
2022-05-08 00:10:26 +0300

    Create permissions.py

--- voltage/permissions.py
+++ voltage/permissions.py
@@ -5,7 +5,6 @@ from typing import Optional
 
 from .flag import FlagBase, FlagValue
 
-
 # https://github.com/revoltchat/revolt.js/blob/master/src/permissions/definitions.ts
 class PermissionsFlag(FlagBase):
     """
@@ -215,3 +214,11 @@ class PermissionsFlag(FlagBase):
         Whether the move members permission is granted.
         """
         return 1 << 34
+
+class Permissions:
+    """
+    A class which represents a member's permissions.
+    """
+    def __init__(self, data: PermissionsOverridePayload):
+        self.allow = PermissionsFlag.new_with_flag(data["a"])
+        self.deny = PermissionsFlag.new_with_flag(data["d"])
