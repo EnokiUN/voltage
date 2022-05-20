eaf925929f21fb9259aae7504dd11ba0602d8cb8
EnokiUN <minekillergaming1@gmail.com>
2022-05-20 19:22:27 +0300

    Merge branch 'devtools' of github.com:enokiun/voltage into devtools

--- voltage/ext/devtools/shell.py
+++ voltage/ext/devtools/shell.py
@@ -24,7 +24,7 @@ class Shell:
         if stdout is None:
             raise RuntimeError("Process doesn't have an stdout")
         # Note to future self: mypy sucks
-        for c in iter(lambda: stdout.readline(), b""): # type: ignore
+        for c in iter(lambda: stdout.readline(), b""):  # type: ignore
             self.loop.call_soon_threadsafe(self.loop.create_task, callback(c.decode("UTF-8"), *args, *kwargs))
 
     def handle_stderr(self, callback: Callable[[str], Awaitable[Any]], *args, **kwargs):
@@ -33,7 +33,7 @@ class Shell:
         stderr = self.process.stderr
         if stderr is None:
             raise RuntimeError("Process doesn't have an stderr")
-        for c in iter(lambda: stderr.readline(), b""): # type: ignore
+        for c in iter(lambda: stderr.readline(), b""):  # type: ignore
             self.loop.call_soon_threadsafe(self.loop.create_task, callback(c.decode("UTF-8"), *args, *kwargs))
 
     async def run(self, callback: Callable[[str], Awaitable[Any]], *args, **kwargs):
