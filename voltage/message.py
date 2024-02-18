d4e73479e464359bb5710f4d52f34b180898b50f
Amy <138383945+amycatgirl@users.noreply.github.com>
2024-02-18 17:03:13 -0400

    fix: duplicate slash when self.server is None

--- voltage/message.py
+++ voltage/message.py
@@ -309,7 +309,7 @@ class Message:
     @property
     def jump_url(self) -> str:
         """Returns a URL that allows the client to jump to the message."""
-        server_segment = "/" if self.server is None else f"/server/{self.server.id}"
+        server_segment = "" if self.server is None else f"/server/{self.server.id}"
         return f"https://app.revolt.chat{server_segment}/channel/{self.channel.id}/{self.id}"
 
     @property
